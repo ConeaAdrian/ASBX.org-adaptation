5ae037bf2fdd41b2160f83115994ac88e9a68e2f
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-05-04 20:21:26 +0300

    create page index

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 @@include("partials/main.js");
 @@include("partials/nav.js");
 @@include("partials/header.js");
+
+$(document).ready(function() {
+    $('.faq-item').addClass('closed'); // adauga clasa "closed" initial pe toate elementele "faq-item"
+    $('.faq-item').click(function() {
+      $(this).toggleClass('open closed');
+      $(this).children('.answer').slideToggle();
+    });
+  });
+   
+  
+  
+
