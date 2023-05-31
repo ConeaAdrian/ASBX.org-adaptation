5137177cee91d404814a3048899893ab342d8db9
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-05-31 14:13:28 +0300

    adaptation last element and create new form

--- src/js/main.js
+++ src/js/main.js
@@ -2,32 +2,3 @@
 @@include("partials/nav.js");
 @@include("partials/header.js");
 @@include("partials/map.js");
-
-
-
-$(document).ready(function() {
-    $('.validation-calculator > div').hide();
-    $('.validation-calculator').hide();
-  
-    $('.client-question__content .button').click(function() {
-      $('.validation-calculator').fadeIn();
-      $('.number-validation').fadeIn();
-      $('body').addClass('locked');
-    });
-  
-    $('.number-validation .button').click(function() {
-      $('.number-validation').fadeOut();
-      $('.code-confirmation').fadeIn();
-    });
-  
-    $('.code-confirmation .button').click(function() {
-      $('.code-confirmation').fadeOut();
-      $('.thank-you').fadeIn();
-    });
-  
-    $('.thank-you .button').click(function() {
-      $('.thank-you').fadeOut();
-      $('.validation-calculator').fadeOut();
-      $('body').removeClass('locked');
-    });
-  });
