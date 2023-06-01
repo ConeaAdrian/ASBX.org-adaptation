4f2a41e77cf714ba42fc16d913f37138a58b0d0f
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-06-01 18:24:30 +0300

    add functional open

--- src/js/partials/claims-selected.js
+++ src/js/partials/claims-selected.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   $('.profession').click(function() {
     $(this).toggleClass('active');
-    $(this).parent().find('.profession__content').slideToggle();
+    $(this).next('.profession__content').slideToggle();
+  });
+
+  // Deschide elementele by default
+  $('.profession').each(function() {
+    $(this).addClass('active');
+    $(this).next('.profession__content').slideDown();
   });
 });
