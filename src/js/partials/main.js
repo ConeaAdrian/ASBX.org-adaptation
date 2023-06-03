78ddb539c52a8bed1ed384d953782e484749fc8b
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-06-03 13:04:21 +0300

    pop-up adaptation

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -78,74 +78,74 @@ $(document).ready(function (e) {
  //--------
 
  
- $(document).ready(function() {
-  $('.popup-claim').hide();
+//  $(document).ready(function() {
+//   $('.popup-claim').hide();
 
-  $('.button-open').click(function() {
-    $('.popup-claim').fadeIn();
-    $('body').addClass('locked');
-  });
+//   $('.button-open').click(function() {
+//     $('.popup-claim').fadeIn();
+//     $('body').addClass('locked');
+//   });
 
-  $('.chase').change(function() {
-    var job = $('#job').val();
-    var start = $('#start').val();
-    var end = $('#end').val();
-    if (job && start && end) {
-      $('.popup-button').removeAttr('disabled');
-      $('.popup-claim').removeClass('error');
-      $('.close-button-alt').hide();
-      $('.button-add').css({
-        'background-color': '#324a74',
-        'border-color': '#324a74',
-        'color': '#ffffff'
-      });
-    } else {
-      $('.popup-button').attr('disabled', 'disabled');
-      $('.close-button-alt').show();
-      $('.button-add').css({
-        'background-color': '#acaaaa',
-        'border-color': '#324A74',
-        'color': ''
-      });
-    }
-  });
+//   $('.chase').change(function() {
+//     var job = $('#job').val();
+//     var start = $('#start').val();
+//     var end = $('#end').val();
+//     if (job && start && end) {
+//       $('.popup-button').removeAttr('disabled');
+//       $('.popup-claim').removeClass('error');
+//       $('.close-button-alt').hide();
+//       $('.button-add').css({
+//         'background-color': '#324a74',
+//         'border-color': '#324a74',
+//         'color': '#ffffff'
+//       });
+//     } else {
+//       $('.popup-button').attr('disabled', 'disabled');
+//       $('.close-button-alt').show();
+//       $('.button-add').css({
+//         'background-color': '#acaaaa',
+//         'border-color': '#324A74',
+//         'color': ''
+//       });
+//     }
+//   });
 
-  $('.popup-button').click(function(e) {
-    e.preventDefault();
-    var job = $('#job').val();
-    var start = $('#start').val();
-    var end = $('#end').val();
-    if (job && start && end) {
-      $('.popup-claim').fadeOut(function() {
-        $('body').removeClass('locked');
-        $('#job').val('');
-        $('#start').val('');
-        $('#end').val('');
-        $('.button-add').css({
-          'background-color': '#acaaaa',
-          'border-color': '#324A74',
-          'color': ''
-        });
-      });
-    } else {
-      $('.popup-claim').addClass('error');
-    }
-  });
+//   $('.popup-button').click(function(e) {
+//     e.preventDefault();
+//     var job = $('#job').val();
+//     var start = $('#start').val();
+//     var end = $('#end').val();
+//     if (job && start && end) {
+//       $('.popup-claim').fadeOut(function() {
+//         $('body').removeClass('locked');
+//         $('#job').val('');
+//         $('#start').val('');
+//         $('#end').val('');
+//         $('.button-add').css({
+//           'background-color': '#acaaaa',
+//           'border-color': '#324A74',
+//           'color': ''
+//         });
+//       });
+//     } else {
+//       $('.popup-claim').addClass('error');
+//     }
+//   });
 
-  $('.close-button').click(function() {
-    $('.popup-claim').fadeOut(function() {
-      $('body').removeClass('locked');
-      $('#job').val('');
-      $('#start').val('');
-      $('#end').val('');
-      $('.button-add').css({
-        'background-color': '#acaaaa',
-        'border-color': '#324A74',
-        'color': ''
-      });
-    });
-  });
-});
+//   $('.close-button').click(function() {
+//     $('.popup-claim').fadeOut(function() {
+//       $('body').removeClass('locked');
+//       $('#job').val('');
+//       $('#start').val('');
+//       $('#end').val('');
+//       $('.button-add').css({
+//         'background-color': '#acaaaa',
+//         'border-color': '#324A74',
+//         'color': ''
+//       });
+//     });
+//   });
+// });
 
 
  
@@ -155,3 +155,32 @@ $(document).ready(function (e) {
  //----------
  
  
+
+ function add_claim(el) {
+  let popup = el.parentElement.parentElement.parentElement.querySelector("#popup-claim");
+  let closeButton = popup.querySelector(".close-button");
+  let addClaimsButton = popup.querySelector(".button-add");
+
+  popup.style.display = "block";
+
+  closeButton.addEventListener("click", function() {
+    popup.style.display = "none";
+  });
+
+  let dropdowns = popup.querySelectorAll(".chase");
+  dropdowns.forEach(function(dropdown) {
+    dropdown.addEventListener("change", function() {
+      let selectedOptions = Array.from(dropdowns).every(function(dropdown) {
+        return dropdown.value !== "";
+      });
+
+      if (selectedOptions) {
+        addClaimsButton.disabled = false;
+        addClaimsButton.style.backgroundColor = "#324A74";
+      } else {
+        addClaimsButton.disabled = true;
+        addClaimsButton.style.backgroundColor = "";
+      }
+    });
+  });
+}
