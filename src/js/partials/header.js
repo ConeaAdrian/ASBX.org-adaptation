036b223dd087dd7189ef0c9b8ffe2fe080e24d74
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-07-07 12:37:16 +0300

    JS function

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,88 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var addButtons = document.querySelectorAll('.add-button');
-    var jobTitleSelect = document.getElementById('jobTitleSelect');
-    var fromYearSelect = document.getElementById('fromYearSelect');
-    var toYearSelect = document.getElementById('toYearSelect');
-  
-    addButtons.forEach(function(addButton) {
-      addButton.addEventListener('click', function(event) {
-        event.preventDefault();
-  
-        var parentForm = this.closest('.jobsite-card');
-        var errorMessage = parentForm.querySelector('.error-message');
-  
-        if (jobTitleSelect.value === 'None' || fromYearSelect.value === 'None' || toYearSelect.value === 'None') {
-          errorMessage.style.display = 'block';
-  
-          setTimeout(function() {
-            errorMessage.style.display = 'none';
-          }, 3000);
+  var addButtons = document.querySelectorAll('.add-button');
+
+  addButtons.forEach(function(addButton) {
+    addButton.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      var parentForm = this.closest('.jobsite-card');
+      var errorMessage = parentForm.querySelector('.error-message');
+      var jobTitleSelect = parentForm.querySelector('#jobTitleSelect');
+      var fromYearSelect = parentForm.querySelector('#fromYearSelect');
+      var toYearSelect = parentForm.querySelector('#toYearSelect');
+
+      if (jobTitleSelect.value === 'None' || fromYearSelect.value === 'None' || toYearSelect.value === 'None') {
+        errorMessage.style.display = 'block';
+
+        setTimeout(function() {
+          errorMessage.style.display = 'none';
+        }, 3000);
+      } else {
+        parentForm.submit(); // Submit the form
+      }
+    });
+    
+
+    var fromYearSelect = addButton.closest('.jobsite-card').querySelector('#fromYearSelect');
+    var toYearSelect = addButton.closest('.jobsite-card').querySelector('#toYearSelect');
+    
+    fromYearSelect.addEventListener('change', function() {
+      var selectedFromYear = parseInt(this.value);
+      var toYearOptions = Array.from(toYearSelect.options);
+
+      toYearOptions.forEach(function(option) {
+        var optionYear = parseInt(option.value);
+        
+
+        if (isNaN(optionYear) || optionYear >= selectedFromYear) {
+          option.style.display = "";
         } else {
-          parentForm.submit(); // Submit the form
+          option.style.display = "none";
         }
       });
     });
-  
-    fromYearSelect.addEventListener('change', function() {
-      var selectedYear = parseInt(this.value);
-  
-      for (var i = 0; i < toYearSelect.options.length; i++) {
-        var option = toYearSelect.options[i];
-        var year = parseInt(option.value);
-  
-        if (year < selectedYear) {
-          option.style.display = 'none'; // Hide the option
-        } else {
-          option.style.display = 'block'; // Show the option
-        }
-      }
-  
-      // Enable or disable the toYearSelect dropdown based on the selected option
-      if (selectedYear === 0) {
-        toYearSelect.disabled = true;
-      } else {
-        toYearSelect.disabled = false;
-      }
-    });
-  
-    fromYearSelect.dispatchEvent(new Event('change')); // Trigger the change event initially
-    
-    if (fromYearSelect.value !== 'None') {
-      fromYearSelect.style.display = 'block';
-    }
-  
-    fromYearSelect.addEventListener('mousedown', function() {
-      if (jobTitleSelect.value === 'None') {
-        return false; // Prevent selection if no job title is selected
-      }
-    });
-  
-    toYearSelect.addEventListener('mousedown', function() {
-      var selectedYear = parseInt(fromYearSelect.value);
-  
-      for (var i = 0; i < toYearSelect.options.length; i++) {
-        var option = toYearSelect.options[i];
-        var year = parseInt(option.value);
-  
-        if (year < selectedYear) {
-          option.disabled = true;
-        } else {
-          option.disabled = false;
-        }
-      }
-    });
-  
-    jobTitleSelect.addEventListener('change', function() {
-      if (jobTitleSelect.value === 'None') {
-        fromYearSelect.disabled = true;
-        toYearSelect.disabled = true;
-      } else {
-        fromYearSelect.disabled = false;
-        toYearSelect.disabled = false;
-      }
-    });
   });
-  
+});
+
 
   document.addEventListener('DOMContentLoaded', function() {
     var addButtons = document.querySelectorAll('.add-button');
