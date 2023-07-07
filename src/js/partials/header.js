c15a74f4dbca71905e8e1f6b3d6a55a7b58903f1
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-07-07 17:07:56 +0300

    ship functional

--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -61,55 +61,28 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+document.addEventListener('DOMContentLoaded', function() {
+  var addButtons = document.querySelectorAll('.add-button');
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var addButtons = document.querySelectorAll('.add-button');
-  
-    addButtons.forEach(function(addButton) {
-      addButton.addEventListener('click', function(event) {
-        event.preventDefault();
-  
-        var parentForm = this.closest('.boiler-card');
-        var errorMessage = parentForm.querySelector('.error-message');
-        var jobTitleSelect = parentForm.querySelector('#select1');
-        var beforeCheckbox = parentForm.querySelector('#beforeCheckbox');
-        var afterCheckbox = parentForm.querySelector('#afterCheckbox');
-  
-        if (jobTitleSelect.value === '' || (beforeCheckbox.checked === false && afterCheckbox.checked === false)) {
-          errorMessage.style.display = 'block';
-  
-          setTimeout(function() {
-            errorMessage.style.display = 'none';
-          }, 3000);
-        } else {
-          parentForm.submit(); // Submit the form
-        }
-      });
+  addButtons.forEach(function(addButton) {
+    addButton.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      var parentForm = this.closest('.add-navy');
+      var errorMessage = parentForm.querySelector('.error-message');
+      var jobTitleSelect = parentForm.querySelector('#select1');
+      var beforeCheckbox = parentForm.querySelector('#beforeCheckbox');
+      var afterCheckbox = parentForm.querySelector('#afterCheckbox');
+
+      if (jobTitleSelect.value === '' || (beforeCheckbox.checked === false && afterCheckbox.checked === false)) {
+        errorMessage.style.display = 'block';
+
+        setTimeout(function() {
+          errorMessage.style.display = 'none';
+        }, 3000);
+      } else {
+        parentForm.submit(); // Submit the form
+      }
     });
   });
-  document.addEventListener('DOMContentLoaded', function() {
-    var addButtons = document.querySelectorAll('.add-button');
-  
-    addButtons.forEach(function(addButton) {
-      addButton.addEventListener('click', function(event) {
-        event.preventDefault();
-  
-        var parentForm = this.closest('.add-navy'); // Am corectat clasa în care cautăm formularul
-        var errorMessage = parentForm.querySelector('.error-message');
-        var jobTitleSelect = parentForm.querySelector('#select1');
-        var beforeCheckbox = parentForm.querySelector('#beforeCheckbox');
-        var afterCheckbox = parentForm.querySelector('#afterCheckbox');
-  
-        if (jobTitleSelect.value === '' || (beforeCheckbox.checked === false && afterCheckbox.checked === false)) {
-          errorMessage.style.display = 'block';
-  
-          setTimeout(function() {
-            errorMessage.style.display = 'none';
-          }, 3000);
-        } else {
-          parentForm.submit(); // Submit the form
-        }
-      });
-    });
-  });
-  
+});
