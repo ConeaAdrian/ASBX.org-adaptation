a53df003c35aea117f28928ea42d5c17949b8ff3
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-07-26 14:31:01 +0300

    adaptation search

--- src/js/partials/result-page.js
+++ src/js/partials/result-page.js
@@ -58,3 +58,29 @@ switchButton.addEventListener('click', function() {
     yourOwnText.style.fontSize = this.checked ? '14px' : '14px';
   }
 });
+
+
+
+
+// (function ($) {
+//   $(function () {
+//     $("nav ul li a:not(:only-child)").click(function (e) {
+//       $(this).siblings(".nav-dropdown").toggle();
+
+//       $(".nav-dropdown").not($(this).siblings()).hide();
+//       e.stopPropagation();
+//     });
+
+//     $("html").click(function () {
+//       $(".nav-dropdown").hide();
+//     });
+
+//     $("#nav-toggle").click(function () {
+//       $("nav ul").slideToggle();
+//     });
+
+//     $("#nav-toggle").on("click", function () {
+//       this.classList.toggle("active");
+//     });
+//   });
+// })(jQuery);
