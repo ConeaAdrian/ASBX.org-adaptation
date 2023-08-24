c3fa33e8fb8adc6c0708f57193ade270d7b962d4
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-08-24 16:42:40 +0300

    Create js for button show

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 @@include("partials/main.js");
 @@include("partials/claims-selected.js");
 @@include("partials/nav.js");
+@@include("partials/functional.js");
 @@include("partials/header.js");
 @@include("partials/result-page.js");
 @@include("partials/search-mobile.js");
 @@include("partials/claims-calculator.js");
 @@include("partials/search.js");
+
+
