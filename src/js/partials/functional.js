3e9606181110346e6f20686d1033a20c1b941b9a
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-08-25 10:32:38 +0300

    new JS

--- src/js/partials/functional.js
+++ src/js/partials/functional.js
@@ -1,24 +1,4 @@
-document.getElementById('ShowButton').addEventListener('click', function() {
-    var hiddenColumns = document.querySelectorAll('.hidden-column');
-    var button = document.getElementById('ShowButton');
-  
-    if (hiddenColumns.length > 0) {
-      for (var i = 0; i < hiddenColumns.length; i++) {
-        hiddenColumns[i].classList.remove('hidden-column');
-      }
-      button.innerText = "Hide";
-    } else {
-      var columnsToHide = document.querySelectorAll('.city-column th:nth-child(n+5), .trust-fund-column td:nth-child(n+5), .job-sites-column td:nth-child(n+5)');
-      for (var j = 0; j < columnsToHide.length; j++) {
-        columnsToHide[j].classList.add('hidden-column');
-      }
-      button.innerText = "Show More";
-    }
-  });
-  
-
-
-  var button = document.getElementById('ShowButton');
+var button = document.getElementById('ShowButton');
 
 if (button) {
   button.addEventListener('click', function() {
