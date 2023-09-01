c61f5383586e1bc40900797b33b9b5d8431b987e
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-09-01 10:27:44 +0300

    adaptation state button

--- src/js/partials/functional.js
+++ src/js/partials/functional.js
@@ -1,23 +1,23 @@
-var button = document.getElementById('ShowButton');
+// var button = document.getElementById('ShowButton');
 
-if (button) {
-  button.addEventListener('click', function() {
-    var hiddenColumns = document.querySelectorAll('.hidden-column');
+// if (button) {
+//   button.addEventListener('click', function() {
+//     var hiddenColumns = document.querySelectorAll('.hidden-column');
   
-    if (hiddenColumns.length > 0) {
-      for (var i = 0; i < hiddenColumns.length; i++) {
-        hiddenColumns[i].classList.remove('hidden-column');
-      }
-      button.innerText = "Hide";
-    } else {
-      var columnsToHide = document.querySelectorAll('.city-column th:nth-child(n+7), .trust-fund-column td:nth-child(n+7), .job-sites-column td:nth-child(n+7)');
-      for (var j = 0; j < columnsToHide.length; j++) {
-        columnsToHide[j].classList.add('hidden-column');
-      }
-      button.innerText = "Show More";
-    }
-  });
-}
+//     if (hiddenColumns.length > 0) {
+//       for (var i = 0; i < hiddenColumns.length; i++) {
+//         hiddenColumns[i].classList.remove('hidden-column');
+//       }
+//       button.innerText = "Hide";
+//     } else {
+//       var columnsToHide = document.querySelectorAll('.city-column th:nth-child(n+7), .trust-fund-column td:nth-child(n+7), .job-sites-column td:nth-child(n+7)');
+//       for (var j = 0; j < columnsToHide.length; j++) {
+//         columnsToHide[j].classList.add('hidden-column');
+//       }
+//       button.innerText = "Show More";
+//     }
+//   });
+// }
 
 
 //--scrop search
