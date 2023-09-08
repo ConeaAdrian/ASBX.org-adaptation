2276fba4ecafdd5e6c91bf3e746516f26215efde
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-09-08 12:57:52 +0300

    element new

--- src/js/partials/functional.js
+++ src/js/partials/functional.js
@@ -53,30 +53,29 @@ document.addEventListener("DOMContentLoaded", function () {
     let answer = question.nextElementSibling;
 
     icon.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
-          <path d="M8.46094 1.95508V15.9551" stroke="#170F49" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M1.46094 8.95508H15.4609" stroke="#170F49" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="3" viewBox="0 0 17 3" fill="none">
+    <path d="M1.46094 1.95508H15.4609" stroke="#4A3AFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
       `;
 
-    // Închide răspunsurile inițial
-    answer.style.display = "none";
+    // Open answers initially
+    answer.style.display = "block";
 
     question.addEventListener("click", function () {
       if (answer.style.display === "block") {
         answer.style.display = "none";
         icon.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="3" viewBox="0 0 17 3" fill="none">
-        <path d="M1.46094 1.95508H15.4609" stroke="#4A3AFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
+          <path d="M8.46094 1.95508V15.9551" stroke="#170F49" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M1.46094 8.95508H15.4609" stroke="#170F49" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
           `;
       } else {
         answer.style.display = "block";
         icon.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
-          <path d="M8.46094 1.95508V15.9551" stroke="#170F49" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M1.46094 8.95508H15.4609" stroke="#170F49" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="3" viewBox="0 0 17 3" fill="none">
+        <path d="M1.46094 1.95508H15.4609" stroke="#4A3AFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
           `;
       }
     });
