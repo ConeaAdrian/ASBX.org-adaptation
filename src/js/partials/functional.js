16937123fa6c8c43cd930ab089739d0bd625a3f6
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-09-11 16:33:10 +0300

    all element adaptation

--- src/js/partials/functional.js
+++ src/js/partials/functional.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     icon.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" width="17" height="3" viewBox="0 0 17 3" fill="none">
-    <path d="M1.46094 1.95508H15.4609" stroke="#4A3AFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1.46094 1.95508H15.4609"  stroke="#170F49" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
       `;
 
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", function () {
         answer.style.display = "block";
         icon.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" width="17" height="3" viewBox="0 0 17 3" fill="none">
-        <path d="M1.46094 1.95508H15.4609" stroke="#4A3AFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M1.46094 1.95508H15.4609"  stroke="#170F49" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
           `;
       }
