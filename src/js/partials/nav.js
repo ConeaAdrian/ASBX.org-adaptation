ac4e0d4b9d9c56bef6e9bf6c5bcd503669671fb2
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-09-20 19:10:04 +0300

    nav final result

--- src/js/partials/nav.js
+++ src/js/partials/nav.js
@@ -1,48 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let navLinks = document.querySelectorAll(".nav-list li a:not(:only-child)");
-  navLinks.forEach(function (link) {
-      link.addEventListener("click", function (e) {
-          e.stopPropagation();
+    let navLinks = document.querySelectorAll("nav ul li a:not(:only-child)");
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            e.stopPropagation();
 
-          let dropdown = this.nextElementSibling;
-          if (dropdown) { 
-              dropdown.style.display = dropdown.style.display !== "block" ? "block" : "none";
+            let dropdown = this.nextElementSibling;
+            if (dropdown) { 
+                dropdown.style.display = 
+                    dropdown.style.display !== "flex" ? "flex" : "none";
 
-              if (dropdown.classList.contains("top-level")) {
-                  let otherDropdowns = document.querySelectorAll(".top-level");
-                  otherDropdowns.forEach(function (d) {
-                      if (d !== dropdown) {
-                          d.style.display = "none";
-                      }
-                  });
-              } else if (dropdown.classList.contains("sub-level")) {
-                  let otherDropdowns = document.querySelectorAll(".sub-level");
-                  otherDropdowns.forEach(function (d) {
-                      if (d !== dropdown) {
-                          d.style.display = "none";
-                      }
-                  });
-              }
-          }
-      });
-  });
+                let dropdowns = document.querySelectorAll(".nav-dropdown");
+                dropdowns.forEach(function (d) {
+                    if (d !== dropdown) {
+                        d.style.display = "none";
+                    }
+                });
+            }
+        });
+    });
 
-  document.querySelector("html").addEventListener("click", function () {
-      let dropdowns = document.querySelectorAll(".nav-dropdown");
-      dropdowns.forEach(function (d) {
-          d.style.display = "none";
-      });
-  });
+    document.querySelector("html").addEventListener("click", function () {
+        let dropdowns = document.querySelectorAll(".nav-dropdown");
+        dropdowns.forEach(function (d) {
+            d.style.display = "none";
+        });
+    });
 
-  let navToggle = document.querySelector("#nav-toggle");
-  navToggle.addEventListener("click", function () {
-      let nav = document.querySelector("nav ul");
-      if (this.classList.contains("active")) {
-          nav.style.display = "none";
-          this.classList.remove("active");
-      } else {
-          nav.style.display = "block";
-          this.classList.add("active");
-      }
-  });
+    let navToggle = document.querySelector("#nav-toggle");
+    navToggle.addEventListener("click", function () {
+        let nav = document.querySelector("nav ul");
+        if (this.classList.contains("active")) {
+            nav.style.display = "none";
+            this.classList.remove("active");
+        } else {
+            nav.style.display = "flex";
+            this.classList.add("active");
+        }
+    });
 });
