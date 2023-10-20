92a8acf35061bd38de559cee0a3811165c118834
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-10-20 17:11:11 +0300

    push new search

--- src/js/partials/nav.js
+++ src/js/partials/nav.js
@@ -1,40 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let navLinks = document.querySelectorAll("nav ul li a:not(:only-child)");
-    navLinks.forEach(function (link) {
-        link.addEventListener("click", function (e) {
-            e.stopPropagation();
+    if (window.innerWidth <= 993) {
+        let navLinks = document.querySelectorAll("nav ul li a:not(:only-child)");
+        navLinks.forEach(function (link) {
+            link.addEventListener("click", function (e) {
+                e.stopPropagation();
 
-            let dropdown = this.nextElementSibling;
-            if (dropdown) { 
-                dropdown.style.display = 
-                    dropdown.style.display !== "flex" ? "flex" : "none";
+                let dropdown = this.nextElementSibling;
+                if (dropdown) { 
+                    dropdown.style.display = 
+                        dropdown.style.display !== "flex" ? "flex" : "none";
 
-                let dropdowns = document.querySelectorAll(".nav-dropdown");
-                dropdowns.forEach(function (d) {
-                    if (d !== dropdown) {
-                        d.style.display = "none";
-                    }
-                });
+                    let dropdowns = document.querySelectorAll(".nav-dropdown");
+                    dropdowns.forEach(function (d) {
+                        if (d !== dropdown) {
+                            d.style.display = "none";
+                        }
+                    });
+                }
+            });
+        });
+        document.querySelector("html").addEventListener("click", function () {
+            let dropdowns = document.querySelectorAll(".nav-dropdown");
+            dropdowns.forEach(function (d) {
+                d.style.display = "none";
+            });
+        });
+    
+        let navToggle = document.querySelector("#nav-toggle");
+        navToggle.addEventListener("click", function () {
+            let nav = document.querySelector("nav ul");
+            if (this.classList.contains("active")) {
+                nav.style.display = "none";
+                this.classList.remove("active");
+            } else {
+                nav.style.display = "flex";
+                this.classList.add("active");
             }
         });
-    });
-
-    document.querySelector("html").addEventListener("click", function () {
-        let dropdowns = document.querySelectorAll(".nav-dropdown");
-        dropdowns.forEach(function (d) {
-            d.style.display = "none";
-        });
-    });
-
-    let navToggle = document.querySelector("#nav-toggle");
-    navToggle.addEventListener("click", function () {
-        let nav = document.querySelector("nav ul");
-        if (this.classList.contains("active")) {
-            nav.style.display = "none";
-            this.classList.remove("active");
-        } else {
-            nav.style.display = "flex";
-            this.classList.add("active");
-        }
-    });
+    }
 });
