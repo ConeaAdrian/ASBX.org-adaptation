008b76288652bcc692f7f5083bc4478876b55bf6
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-10-23 17:16:37 +0300

    add new element

--- src/js/partials/nav.js
+++ src/js/partials/nav.js
@@ -1,41 +1,75 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.innerWidth <= 993) {
-        let navLinks = document.querySelectorAll("nav ul li a:not(:only-child)");
-        navLinks.forEach(function (link) {
-            link.addEventListener("click", function (e) {
-                e.stopPropagation();
+function setupMobileNavigation() {
+    let navLinks = document.querySelectorAll("nav ul li a:not(:only-child)");
+    let diseasesLinks = document.querySelectorAll("a.diseases");
 
-                let dropdown = this.nextElementSibling;
-                if (dropdown) { 
-                    dropdown.style.display = 
-                        dropdown.style.display !== "flex" ? "flex" : "none";
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            e.stopPropagation();
 
-                    let dropdowns = document.querySelectorAll(".nav-dropdown");
-                    dropdowns.forEach(function (d) {
-                        if (d !== dropdown) {
-                            d.style.display = "none";
-                        }
-                    });
-                }
-            });
-        });
-        document.querySelector("html").addEventListener("click", function () {
-            let dropdowns = document.querySelectorAll(".nav-dropdown");
-            dropdowns.forEach(function (d) {
-                d.style.display = "none";
-            });
-        });
-    
-        let navToggle = document.querySelector("#nav-toggle");
-        navToggle.addEventListener("click", function () {
-            let nav = document.querySelector("nav ul");
-            if (this.classList.contains("active")) {
-                nav.style.display = "none";
-                this.classList.remove("active");
-            } else {
-                nav.style.display = "flex";
-                this.classList.add("active");
+            let dropdown = this.nextElementSibling;
+            if (dropdown) { 
+                dropdown.style.display = 
+                    dropdown.style.display !== "flex" ? "flex" : "none";
+
+                let dropdowns = document.querySelectorAll(".nav-dropdown");
+                dropdowns.forEach(function (d) {
+                    if (d !== dropdown) {
+                        d.style.display = "none";
+                    }
+                });
             }
         });
+    });
+
+    diseasesLinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            let dropdown = this.nextElementSibling;
+            if (dropdown) {
+                dropdown.style.display = "flex";
+
+                let dropdowns = document.querySelectorAll(".nav-dropdown");
+                dropdowns.forEach(function (d) {
+                    if (d !== dropdown) {
+                        d.style.display = "none";
+                    }
+                });
+            }
+        });
+    });
+
+    document.querySelector("html").addEventListener("click", function () {
+        let dropdowns = document.querySelectorAll(".nav-dropdown");
+        dropdowns.forEach(function (d) {
+            d.style.display = "none";
+        });
+    });
+
+    let navToggle = document.querySelector("#nav-toggle");
+    navToggle.addEventListener("click", function () {
+        let nav = document.querySelector("nav ul");
+        if (this.classList.contains("active")) {
+            nav.style.display = "none";
+            this.classList.remove("active");
+        } else {
+            nav.style.display = "flex";
+            this.classList.add("active");
+        }
+    });
+}
+
+function setupDesktopNavigation() {
+
+}
+
+function checkWindowSize() {
+    if (window.innerWidth <= 993) {
+        setupMobileNavigation();
+    } else {
+        setupDesktopNavigation();
     }
-});
+}
+
+document.addEventListener("DOMContentLoaded", checkWindowSize);
+window.addEventListener("resize", checkWindowSize);
