1ab9c8209355ff25c75291e5ce73fda8a75e9c03
Adrian <91064451+ConeaAdrian@users.noreply.github.com>
2023-10-26 16:15:42 +0300

    and new clas

--- src/js/partials/search.js
+++ src/js/partials/search.js
@@ -1,34 +1,34 @@
-// async function TestOpen() {
-//     var searchPrincipal = document.getElementById("search-principal");
-//     if (window.innerWidth <= 767) {
-//         if (searchPrincipal.style.display === "none" || searchPrincipal.style.display === "") {
-//             searchPrincipal.style.display = "block";
-//         } else {
-//             searchPrincipal.style.display = "none";
-//         }
-//     }
-// }
+async function TestOpen() {
+    var searchPrincipal = document.getElementById("search-principal");
+    if (window.innerWidth <= 767) {
+        if (searchPrincipal.style.display === "none" || searchPrincipal.style.display === "") {
+            searchPrincipal.style.display = "block";
+        } else {
+            searchPrincipal.style.display = "none";
+        }
+    }
+}
 
-// function closeSearch(button) {
-//     var searchPrincipal = document.getElementById("search-principal");
-//     searchPrincipal.style.display = "none";
-// }
+function closeSearch(button) {
+    var searchPrincipal = document.getElementById("search-principal");
+    searchPrincipal.style.display = "none";
+}
 
-// async function openClaims() {
-//     var claimsSection = document.getElementById("claims-select-mobile");
-//     if (window.innerWidth <= 992) {
-//         if (claimsSection.style.display === "none" || claimsSection.style.display === "") {
-//             claimsSection.style.display = "block";
-//         } else {
-//             claimsSection.style.display = "none";
-//         }
-//     }
-// }
+async function openClaims() {
+    var claimsSection = document.getElementById("claims-select-mobile");
+    if (window.innerWidth <= 992) {
+        if (claimsSection.style.display === "none" || claimsSection.style.display === "") {
+            claimsSection.style.display = "block";
+        } else {
+            claimsSection.style.display = "none";
+        }
+    }
+}
 
-// function closeClaims() {
-//     var claimsSection = document.getElementById("claims-select-mobile");
-//     claimsSection.style.display = "none";
-// }
+function closeClaims() {
+    var claimsSection = document.getElementById("claims-select-mobile");
+    claimsSection.style.display = "none";
+}
 
 let opensearch = false;
 
